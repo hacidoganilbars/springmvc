7c5ecec96dadc3392dba3e3893ab6c0431a22abf
hdilbars <hd.ilbars@hotmail.com>
2018-05-02 15:31:39 +0300

    Ajax

---
+++ ajax/src/main/java/com/doganilbars/models/ProductModel.java
@@ -0,0 +1,22 @@
+package com.doganilbars.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.doganilbars.entity.Product;
+
+public class ProductModel {
+
+	public Product find() {
+		return new Product("p01", "name 1", 20);
+	}
+
+	public List<Product> findAll() {
+		List<Product> products = new ArrayList<Product>();
+		products.add(new Product("p01", "name 1", 20));
+		products.add(new Product("p02", "name 2", 21));
+		products.add(new Product("p03", "name 3", 22));
+		return products;
+	}
+
+}
